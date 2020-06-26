16a055e2536c45f4258ccf8fc431214d0e19f980
sacredwolfon <59648309+sacredwolfon@users.noreply.github.com>
2020-06-26 15:32:39 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pyglet, random
-song = pyglet.media.load(str(random.randint(1, 5)) + '.mp3')
+
+song = pyglet.media.load("../" + str(random.randint(1, 5)) + '.mp3')
 song.play()
 pyglet.app.run()
